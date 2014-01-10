5cbf1dca7957103ba0c686b5e220fbaf1a0a5e03
Jason Gillman Jr <jgillman@liquidweb.com>
2014-01-10 15:49:48 -0500

    It works now

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 php-propLogic
 =============
 
-My attempt at a very basic propositional logic parser in PHP
+My attempt at a very basic propositional logic parser in PHP.
 
-It's quite rough around the edges. Needless to say, there are severe implications of garbage in, garbage out.
+** Supported Operations **
+* Negation (~)
+* Inclusive Disjunction (|)
+* Conjunction (^)
+* Material Implication (>)
 
-Some things to note.
-
-1. When are doing a basic binary comparison (such as A|B or A^C), don't include the parenthesis. At this point, it doesn't return the right value it seems.
-
-However, at this time, operator precedence isn't accounted for. So if you need something like A|B|C, do it like (A|B)|C or whatever your desired precedence is.
-
-Like I said, this is really rough. If you notice any glaring issues outside of the above, let me know.
+** Operator Precedence **:
+Higher number == higher precedence
+4. ~
+3. ^
+2. |
+1. >
--- propLogic.class.php
+++ propLogic.class.php
@@ -8,10 +8,9 @@
 	class propLogic
 	{
 		// General Properties
-		private $bitCount, $propositionCount, $symbols, $propositions;
-		private	$symbolInt, $operatorList, $logicalOperators, $symbolTruthMap, $validFirstChars;
-		private $currentArray;
-
+		private $bitCount, $symbols, $propositions;
+		private	$symbolInt, $validCharacters, $opFunctions, $symbolTruthMap, $validFirstChars;
+		private $currentArray, $stackLow;
 
 		/**
 		 *
@@ -21,114 +20,103 @@
 		 */
 		public function __construct($symbols, $propositions)
 		{
-			$this->bitCount	= count($symbols);
-			$this->propositionCount	=	count($propositions);
-			$this->symbolInt = (pow(2,$this->bitCount) - 1); // Set the symbol integer to the top
+			// Common variable setup
+			$this->bitCount	= count($symbols); // Determine the bit length based on the number of symbols
+			$this->symbolInt = (pow(2,$this->bitCount) - 1); // Set the symbol integer to a value where all bits are true (truth tables usually start with all values being true)
 
-			$this->symbols = $symbols;
-			$this->propositions = $propositions;
+			$this->symbols = $symbols; // Copy to the private property
+			$this->propositions = $propositions; // Copy to the private property
 
-			// Logical Operators
-			$this->operatorList[] = '~'; // Negation
-			$this->operatorList[] = '>'; // Material Implication
-			$this->operatorList[] = '^'; // Conjunction
-			$this->operatorList[] = '|'; // Inclusive Or
-			$this->operatorList[]	= '('; // Begin Grouping
-			$this->operatorList[] = ')'; // End Grouping
+			$this->sanityCheck(); // Verify that the propositions are legit
+			$this->truthMapper(); // Create the array mapping the symbol boolean values for all the integers possible
 
-			$this->logicalOperators['~']	=
-				function($boolean)
+			// Operator setup
+			// Set for a given input operator as the index, the array is operators with lower precedence in the stack
+			$this->stackLow['>'] = ['('];
+			$this->stackLow['|'] = ['(', '>'];
+			$this->stackLow['^'] = ['(', '>', '|'];
+			$this->stackLow['~'] = ['(', '>', '|', '^']; // Unary negation
+
+			// $this->opFunctions will contain the anonymous functions to make things work(TM)
+			$this->opFunctions['>'] =
+				function ($firstBool, $secondBool)
 				{
-					return !$boolean; // Just a simple negation
-				};
-
-			$this->logicalOperators['>']	=
-				function($firstBoolean, $secondBoolean)
-				{
-					if(($firstBoolean == TRUE) AND ($secondBoolean == FALSE))
+					if(($firstBool === TRUE) AND ($secondBool === FALSE))
 					{
-						return FALSE;
+						$result = FALSE;
 					}
 					else
 					{
-						return TRUE;
+						$result = TRUE;
 					}
+					return $result;
 				};
-
-			$this->logicalOperators['^']	=
-				function($firstBoolean, $secondBoolean)
+			$this->opFunctions['|'] =
+				function ($firstBool, $secondBool)
 				{
-					return ($firstBoolean AND $secondBoolean);
+					$result = ($firstBool OR $secondBool);
+					return $result;
 				};
-
-			$this->logicalOperators['|']	=
-				function($firstBoolean, $secondBoolean)
+			$this->opFunctions['^'] =
+				function ($firstBool, $secondBool)
 				{
-					return ($firstBoolean OR $secondBoolean);
+					$result = ($firstBool AND $secondBool);
+					return $result;
 				};
-
-			// End Logical Operators
-
-			$this->sanityCheck(); // Verify we don't have characters that fall outside of the logicalElements array
-			$this->truthMapper(); // Create the array mapping the symbol boolean values for all the integers possible
+			$this->opFunctions['~'] =
+				function ($onlyBool)
+				{
+					$result = !$onlyBool;
+					return $result;
+				};
+				// End operator setup
 		}
 
 		private function sanityCheck() // Does a character check for every proposition
 		{
-			// Accepted input - comprised of the logic operators as well as the symbols
-			foreach($this->operatorList as $operator)
-			{
-				$validElements[] = $operator;
-			}
+			// The following is used to determine valid characters and valid first characters
+			$this->validCharacters[]	=	'~'; // Negation
+			$this->validFirstChars[]	=	'~';
+			$this->validCharacters[]	=	'>'; // Material Implication
+			$this->validCharacters[]	=	'^'; // Conjunction
+			$this->validCharacters[]	=	'|'; // Inclusive Or
+			$this->validCharacters[]	=	'('; // Begin Grouping
+			$this->validFirstChars[]	=	'(';
+			$this->validCharacters[]	=	')'; // End Grouping
 
 			foreach($this->symbols as $symbol)
 			{
-				$validElements[] = $symbol;
-				$this->validFirstChars[] = $symbol; // This is to indicate that symbols are valid first chars in a prop
+				if(in_array($symbol, $this->validCharacters)) // Throw an error and exit
+				{
+					exit('Passed in symbol is already an operator' . "\n");
+				}
+				$this->validCharacters[]	=	$symbol;
+				$this->validFirstChars[]	=	$symbol;
 			}
-			// End Accepted Input
+			// End valid characters
 
-			// Additional valid first chars
-			$this->validFirstChars[] = '~';
-			$this->validFirstChars[] = '(';
-			// End valid first chars
-
-			foreach($this->propositions as $proposition) 
+			// Check each proposition for validity
+			foreach($this->propositions as $proposition)
 			{
-				$tempArray = str_split($proposition);
+				$checkChars	=	str_split($proposition); // Generate the character array
 
-				if(!in_array($tempArray[0], $this->validFirstChars)) // Run a quick check to make sure the first character is valid
-				{
-					exit("Proposition has invalid form\n");
-				}
+				// Make sure the propositions have a valid first character
+				in_array($checkChars[0], $this->validFirstChars)	?:	exit('The proposition ' . $proposition . ' has an invalid first character: ' . $checkChars[0] . "\n");
 
-				foreach($tempArray as $idx => $char)
+				// Make sure all the characters are valid, and while we're at it, do the parenthesis check
+				$parenCount = 0; // Init, even if there aren't parens in the prop
+				foreach($checkChars as $char)
 				{
-					if($char != " ") // Ignore Spaces
-					{
-						if(!in_array($char, $validElements)) // Validate the characters in use are valid elements
-						{
-							exit('Illegal Character: ' . $char . "\n");
-						}
-					}
+					in_array($char, $this->validCharacters)	?:	exit('The proposition ' . $proposition . ' has an invalid character: ' . $char . "\n");
 
-					// Parenthesis balance check
-					if($char == '(')
-					{
-						$openParens++;
-					}
-					if($char == ')')
-					{
-						$openParens--;
-					}
-					// End Parenthesis balance check
+					$char == '('	?	$parenCount++	:	FALSE;
+					$char == ')'	?	$parenCount--	:	FALSE;
+					$parenCount >= 0	?:	exit('Front loaded left parenthesis in proposition: ' . $proposition . "\n");
 				}
-				if($openParens != 0) // We have a mismatch
-				{
-					exit("Parenthesis mismatch\n");
-				}
+				$parenCount == 0	?:	exit('Parenthesis mismatch in proposition: ' . $proposition . "\n");
+				unset($parenCount);
+				unset($checkChars); // Cleanup
 			}
-			unset($validElements); // Cleanup			
 		}
 
 		/**
@@ -138,7 +126,7 @@
 		 */
 		private function truthMapper()
 		{
-			$symbolIntCopy = $this->symbolInt;
+			$symbolIntCopy = $this->symbolInt; // Create a copy so I'm not jacking up the integer that will be used for the parsing
 			while($symbolIntCopy >= 0)
 			{
 				$currBits = str_split(sprintf("%0".$this->bitCount."b", $symbolIntCopy)); // Break the binary string into the individual bits
@@ -148,6 +136,7 @@
 				}
 				--$symbolIntCopy;
 			}
+			unset($symbolIntCopy); // Cleanup
 		}
 
 		public function showMap()
@@ -155,16 +144,8 @@
 			return $this->symbolTruthMap;
 		}
 
-		public function parseLogic()
+		public function parseArgument()
 		{
-			/**
-			 * Order of battle
-			 * 1. Look at integer and use to determine binary (truth) values for symbols
-			 * 2. For each proposition, go as deep as possible and work out
-			 * 3. ?
-			 * 4. Profit!
-			 */
-
 			foreach($this->propositions as $index => $proposition)
 			{
 				$returnArray['byProp'][$index]['proposition'] = $proposition; // Generates an array for storing the value of a proposition grouped by proposition
@@ -176,7 +157,7 @@
 
 				foreach($this->propositions as $propIndex => $proposition)
 				{
-					$propositionWorkingValue = $this->parseProposition($proposition, $truthValues);
+					$propositionWorkingValue = $this->parseProposition($proposition, $truthValues); // Actually parsing the proposition
 
 					$returnArray['byInt'][$workingInt]['propositions'][$propIndex] = 
 						[
@@ -194,6 +175,136 @@
 			//print_r($returnArray); // Debugging
 			$this->currentArray = $returnArray; // Assign this latest run to $this-currentArray
 			return $returnArray;
+		}
+
+		private function infixToRpn($proposition)
+		{
+			$proposition = preg_replace('/\s+/', '', $proposition); // Strip out whitespace
+			$propChars = str_split($proposition); // This way we can analyze and operate on the specific location
+
+			$stack	= array();
+			$output	= array();
+
+			foreach($propChars as $char)
+			{
+				if(in_array($char, $this->symbols))
+				{
+					$output[] = $char;
+				}
+
+				elseif(in_array($char, ['|', '^', '>', '~']))
+				{
+					if(count($stack) == 0)
+					{
+						$stack[] = $char;
+					}
+
+					else
+					{
+						$lowerOps = $this->stackLow[$char]; // Get the operators with a lower precedence
+						while(TRUE)
+						{
+							$currStackItem = end($stack); // Peek at the top of the stack
+
+							if((in_array($currStackItem, $lowerOps)) OR ($currStackItem === NULL) OR ($currStackItem === FALSE)) // A lower precedence operator is in the stack. Leave it there, push the input operator on the stack, and break out of the loop. That, or the current stack item is NULL or FALSE
+							{
+								$stack[] = $char;
+								break;
+							}
+
+							else // We haven't yet seen a lower precedence item in the stack yet, so pop the stack and move it to the output
+							{
+								$output[] = array_pop($stack);
+							}
+						}
+					}
+				}
+
+				elseif($char == '(')
+				{
+					$stack[] = $char;
+				}
+
+				elseif($char == ')')
+				{
+					$currStackItem = end($stack); // Peek at the top of the stack
+					while($currStackItem != '(')
+					{
+						$output[] = array_pop($stack);
+						$currStackItem = end($stack);
+					}
+					array_pop($stack); // Get rid of the left paren
+				}
+			}
+
+			while($tempPop = array_pop($stack)) // Clean out the rest of the stack now that the expression has been run through
+			{
+				$output[] = $tempPop;
+			}
+
+			return $output; // Return the RPN array
+		}
+
+		/**
+		 *
+		 * Parses a single proposition and returns what it evaluates to
+		 *
+		 * @param string	$proposition The proposition to be parsed
+		 * @param array		$truthSet The truthset being used for the current integer (the truth values for the symbols)
+		 *
+		 */
+		public function parseProposition($proposition, $truthset)
+		{
+			/**
+			 * Order of battle
+			 * 1. Infix to postfix (RPN) conversion
+			 * 2. Replace symbols with their boolean values
+			 * 3. Parse the RPN
+			 * 4. ?
+			 * 5. Profit!
+			 */
+
+			// Conversion to RPN
+			$rpn = $this->infixToRpn($proposition);
+
+			// Assign the proper truth value to the symbol
+			foreach($rpn as &$char)
+			{
+				if(in_array($char, $this->symbols))
+				{
+					$char = $truthset[$char];
+				}
+			}
+			unset($char); // The reference needs to be unset, otherwise it seems the second to last element in the array becomes the last when $char is used as the value in the next foreach() loop
+
+			foreach($rpn as $idx => $char) // Evaluate the RPN
+			{
+				if(is_bool($char))
+				{
+					$stack[] = $char;
+				}
+
+				elseif(in_array($char, ['^', '|', '>', '~']))
+				{
+					if($char != '~')
+					{
+						$secondBool	= array_pop($stack);
+						$firstBool	= array_pop($stack);
+
+						$stack[] = $this->opFunctions[$char]($firstBool, $secondBool);
+					}
+					else
+					{
+						$onlyBool = array_pop($stack);
+
+						$stack[] = $this->opFunctions[$char]($onlyBool);
+					}
+				}
+
+				//print_r($stack); // Debugging
+			}
+
+			return array_pop($stack); // Pop the last element and return higher
 		}
 
 		public function asciiTable()
@@ -237,165 +348,9 @@
 
 				echo "\n";
 			}
-
 		}
 
-		/**
-		 *
-		 * Parses a single proposition and returns what it evaluates to
-		 *
-		 * @param string	$proposition The proposition to be parsed
-		 * @param array		$truthSet The truthset being used for the current integer (the truth values for the symbols)
-		 *
-		 */
-		public function parseProposition($proposition, $truthSet)
-		{
-			$proposition = preg_replace('/\s+/', '', $proposition); // Whitespace? Ain't nobody got no time for that!
-			$propChars = str_split($proposition); // Break the proposition down into its base elements - the character
 
-			// Map where the groups start and stop and the level
-			$levelIdx = 0; // Main level
-			$groupMap = Array();// Mark the start and end location(s) of a level via the character index of a string. Parenthesis locations will be the ones tracked
-			$groupMap[$levelIdx] = ['start' => 0, 'end' => (count($propChars) - 1)]; // Main level will always  start at 0 and end at the character count - 1 (this gives the end index)
-			$levelMarker = Array(); // Used to track the last array index for a level when going deeper and coming out if multiple groups exist at that level
-
-			foreach($propChars as $idx => $char)
-			{
-				if($char == '(')
-				{
-					++$levelIdx; // Go down a level
-					$groupMap[$levelIdx][] = ['start' => $idx];
-					$levelMarker[$levelIdx] = (count($groupMap[$levelIdx]) - 1);
-				}
-				if($char == ')')
-				{
-					$groupMap[$levelIdx][$levelMarker[$levelIdx]]['end'] = $idx;
-					--$levelIdx; // Go up a level
-				}
-			}
-			$groupMap = array_reverse($groupMap, TRUE); // Reverse this but preserve keys since we will be working inside out
-
-			//print_r($groupMap);//Debugging
-			// End group mapping
-
-			// Truth value assignments
-			$workingTruthBox = $propChars; // Copy over the array of characters for transformation into truth values
-			foreach($workingTruthBox as $idx => $char)
-			{
-				if(in_array($char, $this->symbols))
-				{
-					$workingTruthBox[$idx] = $truthSet[$char]; // Replace the character with its truth value
-				}
-			}
-			// End truth value assignments
-
-			// Some block here, not sure what will do yet..
-			foreach($groupMap as $groupLevel => $groupLevelContents) // Eval the groups at each level, returning to the one higher when it gets evaled
-			{
-				//echo "Working on group level: " . $groupLevel . "\n"; // Debugging
-				if($groupLevel != 0) // The main group will have different rules
-				{
-					foreach($groupLevelContents as $glcIdx => $endGroup)
-					{
-						//echo "Working on group level: " . $groupLevel . " and group " . $glcIdx . "\n"; // Debugging
-						$groupLength = (($endGroup['end'] - 1) - ($endGroup['start'] + 1)) + 1; // Adjusting for the fact that the start and end locations are where the parens are at, in addition, the +1 is in the event there is only one element inside
-						//$groupData = array_slice($workingTruthBox, $endGroup['start'] + 1, $groupLength, TRUE);
-
-						for($k = ($endGroup['start'] + 1); $k <= ($endGroup['end'] - 1); ++$k) // Because slice didn't seem to be working..
-						{
-							if(isset($workingTruthBox[$k])) // This will skip over indexes that may not be there anymore due to previous runs/modifications
-							{
-								$groupData[$k] = $workingTruthBox[$k];
-								//echo "Importing data at index " . $k ."\n"; // Debugging
-							}
-							else // Debugging
-							{
-								//echo "Key " . $k . " has already been 86'd\n";
-							}
-						}
-						//echo "----------------------------\n"; // Debugging
-						//echo "Working on the following characters:\n"; //Debugging
-						//print_r($groupData); // Debugging
-						reset($groupData); // Reset before we use it
-						//print_r($groupData); // Debugging
-						while($currentData = each($groupData)) // Using each() instead of current() as to distinguish between end of array and boolean false value
-						{
-							prev($groupData); // Since each automatically increments
-							$currentData = $currentData['value']; // Because each() returns an array
-
-							if(is_string($currentData)) // At this point, the only strings should be the logical operators
-							{
-								if($currentData == '~')
-								{
-									$nextData = next($groupData);
-									$result = $this->logicalOperators['~']($nextData); // Negate the following bit o' data
-									prev($groupData); // Pointer Recovery
-								}
-								else
-								{
-									$previousData = prev($groupData);
-										next($groupData); // Pointer Recovery
-									$nextData = next($groupData);
-										prev($groupData); // Pointer Recovery
-									$result = $this->logicalOperators[$currentData]($previousData, $nextData); // Compare the bits of data before and after the operator
-								}
-								
-
-								//echo "Setting index " . $endGroup['start'] . " to " . $result . "\n"; // Debugging
-								for($i = $endGroup['start'] + 1; $i <= $endGroup['end']; ++$i)
-								{
-									if(isset($workingTruthBox[$i])) // This way we don't have PHP complaining about compacting things that already got the works
-									{
-										unset($workingTruthBox[$i]);
-									}
-								}
-								// Replace the group start with the result, and nuke the rest of the range (compact essentially)
-								$workingTruthBox[$endGroup['start']] = $result;
-							}
-							next($groupData);
-						}
-						unset($groupData); // Cleanup
-					}
-				}
-				else // Level 0
-				{
-					while($currentData = each($workingTruthBox)) // Using each() instead of current() as to distinguish between end of array and boolean false value
-					{
-						prev($workingTruthBox); // Since each automatically increments
-						$currentData = $currentData['value']; // Because each() returns an array
-
-						if(is_string($currentData)) // At this point, the only strings should be the logical operators
-						{
-							if($currentData == '~')
-							{
-								$nextData = next($workingTruthBox);
-								$result = $this->logicalOperators['~']($nextData); // Negate the following bit o' data
-								prev($workingTruthBox); // Pointer Recovery
-							}
-							else
-							{
-								$previousData = prev($workingTruthBox);
-									next($workingTruthBox); // Pointer Recovery
-								$nextData = next($workingTruthBox);
-									prev($workingTruthBox); // Pointer Recovery
-								$result = $this->logicalOperators[$currentData]($previousData, $nextData); // Compare the bits of data before and after the operator
-							}
-							
-							// Replace the group start with the result, and nuke the rest of the range (compact essentially)
-							$propositionResult = $result;
-						}
-						next($workingTruthBox);
-					}
-
-					//print_r($groupMap); // Debugging
-					//echo "Level 0 left at:\n"; // Debugging
-					//print_r($workingTruthBox); // Debugging
-				}
-			}
-			// echo "Proposition result evaluates to : " . $propositionResult . "\n"; // Debugging
-			return $propositionResult; // Return it
-			// End some block
-		}
 
 		public function symbolRunner()
 		{
